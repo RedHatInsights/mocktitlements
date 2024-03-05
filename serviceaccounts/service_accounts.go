86182bd425553782426981ec1addf2e6303b8e69
Matt Knop <mknop@redhat.com>
2024-03-05 15:28:59 -0700

    adding todo attributes in struct

--- serviceaccounts/service_accounts.go
+++ serviceaccounts/service_accounts.go
@@ -28,30 +28,11 @@ func init() {
 }
 
 type ServiceAccounts struct {
-	Username      string `json:"username"`
-	ID            int    `json:"id"`
-	Email         string `json:"email"`
-	FirstName     string `json:"first_name"`
-	LastName      string `json:"last_name"`
-	AccountNumber string `json:"account_number"`
-	AddressString string `json:"address_string"`
-	IsActive      bool   `json:"is_active"`
-	IsOrgAdmin    bool   `json:"is_org_admin"`
-	IsInternal    bool   `json:"is_internal"`
-	Locale        string `json:"locale"`
-	OrgID         int    `json:"org_id"`
-	DisplayName   string `json:"display_name"`
-	Type          string `json:"type"`
-	Entitlements  string `json:"entitlements"`
+	// TODO: add attributes
 }
 
 type serviceAccountSpec struct {
-	Username   string              `json:"username"`
-	Enabled    bool                `json:"enabled"`
-	FirstName  string              `json:"firstName"`
-	LastName   string              `json:"lastName"`
-	Email      string              `json:"email"`
-	Attributes map[string][]string `json:"attributes"`
+	// TODO: add attributes
 }
 
 func ServiceAccountHandler(w http.ResponseWriter, r *http.Request, k *http.Client) {
